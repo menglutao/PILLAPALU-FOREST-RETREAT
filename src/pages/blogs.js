b5097cd231bf6e9600b3b2a9dee31515274dd6ff
Menglu <azraeltml@gmail.com>
2022-08-24 11:56:57 +0200

    mdx posts shown on blog page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -9,10 +9,13 @@ const BlogPage = ({data}) => {
     <Layout pageTitle="My Blog Posts">
        <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map(node => (
+         <article key = {node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+
+          </article>
         ))
       }
       </ul>
@@ -21,13 +24,18 @@ const BlogPage = ({data}) => {
 }
 
 export const query = graphql`   
-    query {
-        allFile {
-            nodes {
-              name
-            }
+  query  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
+      }
     }
+  }
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
